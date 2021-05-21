1b0ae15cf6e234d36ca1b5ba0e287376d3814fdf
Fasuplusplus <facundo_forte@hotmail.com>
2021-05-21 02:28:02 -0300

    commit

---
+++ Countto1E9.cpp
@@ -0,0 +1,16 @@
+// Hello World
+#include <cstdio>
+#include <iostream>
+void count(int limit)
+{
+    int Fium = 0;
+    for (int i = Fium; i <= limit; i++)
+    {
+        Fium = i;
+    }
+    printf("done");
+}
+int main(){
+    count(1000000000);
+    return 0;
+}
